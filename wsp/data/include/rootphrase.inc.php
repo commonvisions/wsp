c12792c19a255cf17a2faea5cc1818ae301b064e
Stefan Roscher <s.haendler@covi.de>
2025-01-08 18:05:30 +0100

    Includes

---
+++ wsp/data/include/rootphrase.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * blowfish and xtea passphrase
+ * @author system
+ * @since 4.0
+ * @version 6.7
+ * @lastchange 2018-10-19
+ */
+
+$wspvars['rootphrase'] = "f317d95021b4aa4cf053df5635911afe";
+?>
---
+++ wsp/data/include/usestat.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @since 3.1
+ * @version 7.1735586093
+ * @lastchange 2024-12-30
+ */
+
+$_SESSION['wspvars']['devstat'] = false; // true | false
+$_SESSION['wspvars']['showdeverrors'] = false; // true | false
+$_SESSION['wspvars']['logdeverrors'] = false; // true | false
+$_SESSION['wspvars']['showdevmsg'] = false; // true | false
+$_SESSION['wspvars']['devcontent'] = false; // true | false
+$_SESSION['wspvars']['xajaxdebug'] = false; // true | false
+$_SESSION['wspvars']['debugcontent'] = false; // true | false
+$_SESSION['wspvars']['showsql'] = false; // true | false
+$_SESSION['wspvars']['displaystyle'] = "text" // iconized | text
+
+// EOF ?>
